e22008fb3026fabe6f41dd421853e384690222c5
Harianto van Insulinde <harianto@pop-os.localdomain>
2020-02-21 07:26:00 +0100

    Refactored

--- src/routes/valtech.v1.js
+++ src/routes/valtech.v1.js
@@ -1,334 +1,200 @@
-const
-  readFile = require('../lib/readFile'),
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  path = require('path'),
-  router = express.Router(),
-  jsonpath = path.join(__dirname, '..', 'data')
+const readFile = require('../lib/readFile'),
+	express = require('express'),
+	bodyParser = require('body-parser'),
+	path = require('path'),
+	router = express.Router(),
+	jsonpath = path.join(__dirname, '..', 'data')
 
 console.log(__dirname)
 
-const
-  dataJobsList = require('../data/jobs.list.json'),
+const dataJobsList = require('../data/jobs.list.json'),
 	dataJobsFilters = require('../data/jobs.filters.json')
 
-
-const
-  convertNaN = (value) => !isNaN(value) ? +value : value,
-
-  sortByDate = (a, b) => new Date(b.publishedDate) - new Date(a.publishedDate),
-  
-  convertPropertyTypeToArray = (object, key) => {
-    if (object.hasOwnProperty(key) && !Array.isArray(object[key])) {
-      object[key] = [convertNaN(object[key])]
-    }
-    return object
-  },
-
-	uniqueTagsFromItems = (items) => {
+const convertNaN = value => (!isNaN(value) ? +value : value),
+	sortByDate = (a, b) => new Date(b.publishedDate) - new Date(a.publishedDate),
+	convertPropertyTypeToArray = (object, key) => {
+		if (object.hasOwnProperty(key) && !Array.isArray(object[key])) {
+			object[key] = [convertNaN(object[key])]
+		}
+		return object
+	},
+	uniqueTagsFromItems = items => {
 		let tags = []
 		items.forEach(item => tags.push(...item.tags))
 
-		return tags
-		.filter((value, index, self) => self.indexOf(value) === index)
-  },
+		return tags.filter((value, index, self) => self.indexOf(value) === index)
+	},
+	returnFilteredData = (query, files) => {
+		const { dataFilter, dataList } = files
 
-  returnFilteredData = (query, files) => {
-    const { dataFilter, dataList } = files
+		query = convertPropertyTypeToArray(query, 'filter')
 
-    query = convertPropertyTypeToArray(query, 'filter')
-    
-    const
-      sortedByDate = dataList.sort(sortByDate)
+		const sortedByDate = dataList.sort(sortByDate)
 
-    const mustContain = []
-    dataFilter.forEach(item => {
-      const slug = convertNaN(item.value)
-      if (query.hasOwnProperty(slug) && query[slug]) mustContain.push(query[slug])
-    })
+		const mustContain = []
+		dataFilter.forEach(item => {
+			const slug = convertNaN(item.value)
+			if (query.hasOwnProperty(slug) && query[slug])
+				mustContain.push(query[slug])
+		})
 
-    if (query.hasOwnProperty('filter')) {
-      if (Array.isArray(query.filter)) {
-        mustContain.push(...query.filter)
-      } else {
-        mustContain.push(query.filter)
-      }
-    }
-    // if(query.hasOwnProperty('filter') && !Array.isArray(query.filter)) mustContain.push(query.filter)
-    
+		if (query.hasOwnProperty('filter')) {
+			if (Array.isArray(query.filter)) {
+				mustContain.push(...query.filter)
+			} else {
+				mustContain.push(query.filter)
+			}
+		}
+		// if(query.hasOwnProperty('filter') && !Array.isArray(query.filter)) mustContain.push(query.filter)
 
-    const filteredItems = sortedByDate.filter(item => {
-      let found = true
-      mustContain.forEach(n => {
-        found *= item.tags.indexOf(convertNaN(n)) > -1
-      })
-      return Boolean(found)
-    })
-    let pageOffset = convertNaN(query.offset) || 0,
-      pageLimit = convertNaN(query.limit) || 6,
-      length = filteredItems.length,
-      pageCurrent = Math.floor(pageOffset / pageLimit) + 1,
-      pageTotal = Math.ceil(length / pageLimit),
-      prevOffset = pageCurrent > 1 ? { offset: pageOffset - pageLimit, limit: pageLimit } : pageOffset > 0 && pageOffset < pageLimit ? { offset: 0, limit: pageLimit } : null,
-      nextOffset = pageCurrent < pageTotal ? { offset: pageOffset + pageLimit, limit: pageLimit } : null,
-      list = [...filteredItems].splice(pageOffset, pageLimit),
-      nextQuery = {}
+		const filteredItems = sortedByDate.filter(item => {
+			let found = true
+			mustContain.forEach(n => {
+				found *= item.tags.indexOf(convertNaN(n)) > -1
+			})
+			return Boolean(found)
+		})
+		let pageOffset = convertNaN(query.offset) || 0,
+			pageLimit = convertNaN(query.limit) || 6,
+			length = filteredItems.length,
+			pageCurrent = Math.floor(pageOffset / pageLimit) + 1,
+			pageTotal = Math.ceil(length / pageLimit),
+			prevOffset =
+				pageCurrent > 1
+					? { offset: pageOffset - pageLimit, limit: pageLimit }
+					: pageOffset > 0 && pageOffset < pageLimit
+					? { offset: 0, limit: pageLimit }
+					: null,
+			nextOffset =
+				pageCurrent < pageTotal
+					? { offset: pageOffset + pageLimit, limit: pageLimit }
+					: null,
+			list = [...filteredItems].splice(pageOffset, pageLimit),
+			nextQuery = {}
 
-    delete query.offset
-    delete query.limit
+		delete query.offset
+		delete query.limit
 
-    const data = {
-      filters: dataFilter,
-      page: {
-        itemTotal: length,
-        itemLength: list.length,
-        pageOffset,
-        pageLimit,
-        pageCurrent,
-        pageTotal,
-        pageQuery: query,
-        pagePrevQuery: prevOffset ? {...query, ...prevOffset} : null,
-        pageNextQuery: nextOffset ? {...query, ...nextOffset} : null,
-        tagsUnique: uniqueTagsFromItems(filteredItems)
-      },
-      list
-    }
-    switch (query.listtype) {
-      case 'listonly':
-        return list
-        break
-      case 'all':
-        return sortedByDate
-        break
-      default:
-        return data
-    }
-  }
+		const data = {
+			filters: dataFilter,
+			page: {
+				itemTotal: length,
+				itemLength: list.length,
+				pageOffset,
+				pageLimit,
+				pageCurrent,
+				pageTotal,
+				pageQuery: query,
+				pagePrevQuery: prevOffset ? { ...query, ...prevOffset } : null,
+				pageNextQuery: nextOffset ? { ...query, ...nextOffset } : null,
+				tagsUnique: uniqueTagsFromItems(filteredItems)
+			},
+			list
+		}
+		switch (query.listtype) {
+			case 'listonly':
+				return list
+				break
+			case 'all':
+				return sortedByDate
+				break
+			default:
+				return data
+		}
+	},
+	queryFilterFiles = ({ label, query }) => {
+		const filtersFile = path.join(jsonpath, `${label}.filters.json`),
+			listFile = path.join(jsonpath, `${label}.list.json`)
+
+		return Promise.all([readFile(filtersFile), readFile(listFile)])
+			.then(values => ({
+				dataFilter: JSON.parse(values[0]),
+				dataList: JSON.parse(values[1])
+			}))
+			.then(returnFilteredData.bind(null, query))
+	},
+	listId = label => (req, res, next) => {
+		const listFile = path.join(jsonpath, `${label}.list.json`)
+
+		return Promise.resolve(readFile(listFile))
+			.then(data => JSON.parse(data))
+			.then(dataList => {
+				const { id } = req.params
+				return {
+					item: dataList.find(item => item.id == id) || {}
+				}
+			})
+			.then(res.json.bind(res))
+	},
+	listAll = label => (req, res, next) => {
+		const listFile = path.join(jsonpath, `${label}.list.json`)
+
+		return Promise.resolve(readFile(listFile))
+			.then(list => ({ list: JSON.parse(list) }))
+			.then(res.json.bind(res))
+	},
+	listFilteredAll = label => (req, res, next) => {
+		const query = { ...req.query, ...req.body }
+		console.log(JSON.stringify({ query }))
+
+		return Promise.resolve(queryFilterFiles({ label, query })).then(
+			res.json.bind(res)
+		)
+	}
 
 router.use(bodyParser.json())
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Valtech API')
+	res.send('Valtech API')
 })
 
 router.all('/log', (req, res, next) => {
-  const query = {...req.query, ...req.body}
-
-  res.json(query)
+	const query = { ...req.query, ...req.body }
+	res.json(query)
 })
+
 router.get('/v1/i18n', (req, res, next) => {
-  const
-    i18nFile = path.join(jsonpath, 'valtech.i18n.json')
+	const filePath = path.join(jsonpath, 'valtech.i18n.json')
 
-  return Promise.all([readFile(i18nFile)])
-  .then(values => ({
-    dataI18n: JSON.parse(values[0])
-  }))
-  .then(files => {
-    const { dataI18n } = files
-    return {
-      i18n: dataI18n
-    }
-  })
-  // .then(json => {console.log(json); return json})
-  .then(res.json.bind(res))
+	return Promise.resolve(readFile(filePath))
+		.then(i18n => ({ i18n: JSON.parse(i18n) }))
+		.then(res.json.bind(res))
 })
 
-router.get('/v1/whitepapers/id/:id', (req, res, next) => {
-  const
-    filtersFile = path.join(jsonpath, 'whitepapers.filters.json'),
-    listFile = path.join(jsonpath, 'whitepapers.list.json')
+router
+	.get('/v1/insights/id/:id', listId('insights'))
+	.use('/v1/insights', (req, res, next) => {
+		let query = req.query
 
-  return Promise.all([readFile(filtersFile), readFile(listFile)])
-  .then(values => ({
-    dataFilter: JSON.parse(values[0]),
-    dataList: JSON.parse(values[1])
-  }))
-  .then(files => {
-    const { id } = req.params
-    const { dataList } = files
-    const item = dataList.find(item => item.id == id) || {}
-    return {
-      item
-    }
-  })
-  // .then(json => {console.log(json); return json})
-  .then(res.json.bind(res))
-})
+		if (!query.hasOwnProperty('limit')) {
+		}
 
-router.all('/v1/whitepapers', (req, res, next) => {
-  const query = {...req.query, ...req.body}
-  console.log(JSON.stringify(query))
+		if (
+			!query.hasOwnProperty('offset') ||
+			(query.hasOwnProperty('offset') &&
+				!isNaN(query.offset) &&
+				query.offset == 0)
+		) {
+			query = { ...query, ...{ limit: 13 } }
+		} else {
+			query = { ...query, ...{ limit: 6 } }
+		}
 
-  const
-    filtersFile = path.join(jsonpath, 'whitepapers.filters.json'),
-    listFile = path.join(jsonpath, 'whitepapers.list.json')
+		return Promise.resolve(queryFilterFiles({ label: 'insights', query })).then(
+			res.json.bind(res)
+		)
+	})
 
-  return Promise.all([readFile(filtersFile), readFile(listFile)])
-  .then(values => ({
-    dataFilter: JSON.parse(values[0]),
-    dataList: JSON.parse(values[1])
-  }))
-  .then(returnFilteredData.bind(null, query))
-  // .then(json => {console.log(json); return json})
-  .then(res.json.bind(res))
-})
+router
+	.all('/v1/whitepapers/id/:id', listId('whitepapers'))
+	.all('/v1/whitepapers', listFilteredAll('whitepapers'))
+	.all('/v1/cases/id/:id', listId('cases'))
+	.all('/v1/cases', listFilteredAll('cases'))
+	.all('/v1/jobs/id/:id', listId('jobs'))
+	.all('/v1/jobs', listFilteredAll('jobs'))
 
-router.get('/v1/insights/id/:id', (req, res, next) => {
-  const
-    filtersFile = path.join(jsonpath, 'insights.filters.json'),
-    listFile = path.join(jsonpath, 'insights.list.json')
-
-  return Promise.all([readFile(filtersFile), readFile(listFile)])
-  .then(values => ({
-    dataFilter: JSON.parse(values[0]),
-    dataList: JSON.parse(values[1])
-  }))
-  .then(files => {
-    const { id } = req.params
-    const { dataList } = files
-    const item = dataList.find(item => item.id == id) || {}
-    return {
-      item
-    }
-  })
-  .then(res.json.bind(res))
-})
-
-router.use('/v1/insights', (req, res, next) => {
-  let query = req.query
-
-  if (!query.hasOwnProperty('limit')) {
-
-  }
-  if (!query.hasOwnProperty('offset')
-  || query.hasOwnProperty('offset') 
-  && !isNaN(query.offset)
-  && (query.offset == 0)) {
-    query = {...query, ...{
-      limit: 13
-    }}
-  } else {
-    query = {...query, ...{
-      limit: 6
-    }}
-  }
-
-  
-  const
-    filtersFile = path.join(jsonpath, 'insights.filters.json'),
-    listFile = path.join(jsonpath, 'insights.list.json')
-
-  return Promise.all([readFile(filtersFile), readFile(listFile)])
-  .then(values => ({
-    dataFilter: JSON.parse(values[0]),
-    dataList: JSON.parse(values[1])
-  }))
-  .then(returnFilteredData.bind(null, query))
-  .then(res.json.bind(res))
-})
-
-router.all('/v1/cases/id/:id', (req, res, next) => {
-  const
-    filtersFile = path.join(jsonpath, 'cases.filters.json'),
-    listFile = path.join(jsonpath, 'cases.list.json')
-
-  return Promise.all([readFile(filtersFile), readFile(listFile)])
-  .then(values => ({
-    dataFilter: JSON.parse(values[0]),
-    dataList: JSON.parse(values[1])
-  }))
-  .then(files => {
-    const { id } = req.params
-    const { dataList } = files
-    const item = dataList.find(item => item.id == id) || {}
-    return {
-      item
-    }
-  })
-  .then(res.json.bind(res))
-})
-
-router.all('/v1/cases', (req, res, next) => {
-  const query = {...req.query, ...req.body}
-  console.log(JSON.stringify(query))
-
-  const
-    filtersFile = path.join(jsonpath, 'cases.filters.json'),
-    listFile = path.join(jsonpath, 'cases.list.json')
-
-  return Promise.all([readFile(filtersFile), readFile(listFile)])
-  .then(values => ({
-    dataFilter: JSON.parse(values[0]),
-    dataList: JSON.parse(values[1])
-  }))
-  .then(returnFilteredData.bind(null, query))
-  // .then(json => {console.log(json); return json})
-  .then(res.json.bind(res))
-})
-
-router.get('/v1/jobs/id/:id', (req, res, next) => {
-  const
-    filtersFile = path.join(jsonpath, 'jobs.filters.json'),
-    listFile = path.join(jsonpath, 'jobs.list.json')
-
-  return Promise.all([readFile(filtersFile), readFile(listFile)])
-  .then(values => ({
-    filters: JSON.parse(values[0]),
-    list: JSON.parse(values[1])
-  }))
-  .then(files => {
-    const { id } = req.params
-    const { list } = files
-    const item = list.find(item => item.id == id) || {}
-    return {
-      item
-    }
-  })
-  .then(res.json.bind(res))
-})
-
-router.all('/v1/jobs', (req, res, next) => {
-  const
-    filtersFile = path.join(jsonpath, 'jobs.filters.json'),
-    listFile = path.join(jsonpath, 'jobs.list.json')
-
-  return Promise.all([readFile(filtersFile), readFile(listFile)])
-  .then(values => ({
-    filters: JSON.parse(values[0]),
-    list: JSON.parse(values[1])
-  }))
-  .then(res.json.bind(res))
-})
-
-router.get('/v1/contacts/id/:id', (req, res, next) => {
-  const
-    listFile = path.join(jsonpath, 'offices.list.json')
-
-  return Promise.all([readFile(listFile)])
-  .then(values => ({
-    list: JSON.parse(values[0])
-  }))
-  .then(files => {
-    const { id } = req.params
-    const { list } = files
-    const item = list.find(item => item.id == id) || {}
-    return {
-      item
-    }
-  })
-  .then(res.json.bind(res))
-})
-
-router.all('/v1/contacts', (req, res, next) => {
-  const
-    listFile = path.join(jsonpath, 'offices.list.json')
-
-  return Promise.all([readFile(listFile)])
-  .then(values => ({
-    list: JSON.parse(values[0])
-  }))
-  .then(res.json.bind(res))
-})
+router
+	.get('/v1/contacts/id/:id', listId('offices'))
+	.all('/v1/contacts', listAll('offices'))
 
 module.exports = router
